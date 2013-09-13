492496aff317b118b11059296c5c8f78bb14d730
Dino Fancellu <dino@felstar.com>
2013-09-13 22:12:47 +0100

    Added a /flowers route that emits some XML

--- src/main/scala/com/felstar/playpen/scalatra/MyScalatra.scala
+++ src/main/scala/com/felstar/playpen/scalatra/MyScalatra.scala
@@ -4,6 +4,7 @@ import org.scalatra._
 import java.net.URL
 import org.scalatra.scalate.ScalateSupport
 
+
 class MyScalatra extends ScalatraServlet with ScalateSupport {
 
   get("/") {
@@ -18,4 +19,20 @@ class MyScalatra extends ScalatraServlet with ScalateSupport {
     ssp("woof.ssp","date" -> new java.util.Date)
   } 
   
+  case class Flower(slug: String, name: String) {
+    def toXML= <flower name={name}>{slug}</flower>
+  }
+  
+   val all = List(
+      Flower("yellow-tulip", "Yellow Tulip"),
+      Flower("red-rose", "Red & Rose"),
+      Flower("black-rose", "Black Rose"))
+   
+  get("/flowers"){
+     contentType="text/xml"
+    <flowers> 
+      { all.map(_.toXML) }
+     </flowers>
+  } 
+   
 }
