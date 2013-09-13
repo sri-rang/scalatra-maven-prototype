db740ac36e6237d75490cd19d25f16a73faa673b
Dino Fancellu <dino@felstar.com>
2013-09-13 15:08:17 +0100

    Put in a little SSP example code, nice to use if you know JSP
    http://scalate.fusesource.org/documentation/ssp-reference.html

--- src/main/scala/com/felstar/playpen/scalatra/MyScalatra.scala
+++ src/main/scala/com/felstar/playpen/scalatra/MyScalatra.scala
@@ -5,26 +5,17 @@ import java.net.URL
 import org.scalatra.scalate.ScalateSupport
 
 class MyScalatra extends ScalatraServlet with ScalateSupport {
+
   get("/") {
-    <h1>Hello, world! Me !!!xxx</h1>
+    <html>
+     <h1>Hello, world!</h1>
+     <a href="woof">Try an SSP Page</a>
+	</html>
   }
+  
    get("/woof") {
-    <h1>Woof!!!!!!zzzx</h1>
-  }
-   /*
-   notFound {
-    // If no route matches, then try to render a Scaml template
-    val templateBase = requestPath match {
-      case s if s.endsWith("/") => s + "index"
-      case s => s
-    }
-    val templatePath = "/WEB-INF/scalate/templates/" + templateBase + ".scaml"
-    servletContext.getResource(templatePath) match {
-      case url: URL => 
-        contentType = "text/html"
-        templateEngine.layout(templatePath)    
-    } 
-  }
-  * */
+     contentType="text/html"
+    ssp("woof.ssp","date" -> new java.util.Date)
+  } 
   
 }
